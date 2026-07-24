1c5564d674577087f9d9ca27b8b5fa7e572ca31e
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2026-07-24 07:59:12 +0000

    Revert "PixelPropsUtils: Exclude recorder from pixel props"
    This reverts commit 055a6bfe53aa0141971b1b758b3dd3c38b42c24a.

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -77,8 +77,7 @@ public class PixelPropsUtils {
         "com.google.ar.core",
         "com.google.android.youtube",
         "com.google.android.apps.youtube.kids",
-        "com.google.android.apps.youtube.music",
-        "com.google.android.apps.recorder"
+        "com.google.android.apps.youtube.music"
     };
 
     private static volatile boolean sIsGms = false;

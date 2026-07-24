96623aa07bd96a21182921b46018e49679ce4c0b
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2026-07-24 07:54:06 +0000

    Revert "PixelPropsUtils: Spoof Pixel 7 Pro to SMS Organizer"
    This reverts commit 2ffe17d8abb42304681a1a0bf11f529f54760362.

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -68,7 +68,6 @@ public class PixelPropsUtils {
             "com.android.chrome",
             "com.android.vending",
             "com.breel.wallpapers20",
-            "com.microsoft.android.smsorganizer",
             "com.nothing.smartcenter"
     };
 

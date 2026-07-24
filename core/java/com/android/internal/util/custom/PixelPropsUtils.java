1f9fbfc2ac444305fa42d40e3f0fffed07be1483
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2026-07-24 07:58:27 +0000

    Revert "PixelPropsUtils: Apply key attestation workaround to Play Store as well"
    This reverts commit 2fa4b527dee1fe4d1a8fc8a1dc2ccb7ce70a34f2.

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -62,7 +62,6 @@ public class PixelPropsUtils {
 
     private static final String[] extraPackagesToChange = {
             "com.android.chrome",
-            "com.android.vending",
             "com.breel.wallpapers20"
     };
 
@@ -89,7 +88,6 @@ public class PixelPropsUtils {
     };
 
     private static volatile boolean sIsGms = false;
-    private static volatile boolean sIsFinsky = false;
 
     static {
         propsToKeep = new HashMap<>();
@@ -130,9 +128,6 @@ public class PixelPropsUtils {
 
             if (packageName.equals("com.google.android.apps.photos")) {
                 propsToChange.putAll(propsToChangePixelXL);
-            } else if (packageName.equals("com.android.vending")) {
-                sIsFinsky = true;
-                return;
             } else {
                 if (Arrays.asList(packagesToChangePixel7Pro).contains(packageName)) {
                     propsToChange.putAll(propsToChangePixel7Pro);
@@ -190,11 +185,6 @@ public class PixelPropsUtils {
     public static void onEngineGetCertificateChain() {
         // Check stack for SafetyNet
         if (sIsGms && isCallerSafetyNet()) {
-            throw new UnsupportedOperationException();
-        }
-
-        // Check stack for PlayIntegrity
-        if (sIsFinsky) {
             throw new UnsupportedOperationException();
         }
     }
